c4da7345b949575957377dc1627152af1fea528a
mario malak <93185168+mariomalak1@users.noreply.github.com>
2023-07-10 22:48:31 +0300

    create login

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -12,3 +12,25 @@ class RegisterSerializer(serializers.Serializer):
 		if user:
 			raise serializers.ValidationError("this username is already taken")
 		return username
+
+	def create(self, validated_data):
+		new_user = User.objects.create(username=validated_data.get("username"))
+		new_user.set_password(validated_data.get("password"))
+		if validated_data.get("last_name"):
+			new_user.last_name = validated_data.get("last_name")
+		if validated_data.get("first_name"):
+			new_user.first_name = validated_data.get("first_name")
+		new_user.save()
+
+class LoginSerializer(serializers.Serializer):
+	username = serializers.CharField(max_length=150)
+	password = serializers.CharField(max_length=150)
+
+
+	def validate(self, data):
+		user = User.objects.filter(username=data.get("username")).first()
+		if not user:
+			raise serializers.ValidationError("Username Not Found")
+		return data 
+
+	
--- accounts/views.py
+++ accounts/views.py
@@ -1,28 +1,37 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login as django_login
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
+from rest_framework.authtoken.models import Token
 
-
-from .serializers import RegisterSerializer
+from .serializers import RegisterSerializer, LoginSerializer
 # Create your views here.
 
 
-@api_view(["POST"])
+@api_view(["GET"])
 def register(request):
-	if request.method == "POST":
+	if request.method == "GET":
 		data = request.data
 		serializer = RegisterSerializer(data=data)
 		if serializer.is_valid():
-			new_user = User.objects.create(username=serializer.data.get("username"))
-			new_user.set_password(serializer.data.get("password"))
-			if serializer.data.get("last_name"):
-				new_user.last_name = serializer.data.get("last_name")
-			if serializer.data.get("first_name"):
-				new_user.first_name = serializer.data.get("first_name")
-			new_user.save()
+			serializer.create(serializer.data)
 			return Response(serializer.data, status=status.HTTP_201_CREATED)
 		else:
 			return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+@api_view(["GET"])
+def login(request):
+	data = request.data
+	serializer = LoginSerializer(data=data)
+	if serializer.is_valid():
+		user = authenticate(username=serializer.data.get("username"), password=serializer.data.get("password"))
+		django_login(request, user=user)
+
+		token = Token.objects.filter(user=user).first()
+		if not token:
+			token = Token.objects.create(user = user)
+		return Response({"token": str(token)}, status=status.HTTP_200_OK)
+	else:
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
